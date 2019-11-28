0f520052d53184f9e66f7b128c3b18d9c36109ee
pandakst <37198167+PandaCdC@users.noreply.github.com>
2019-11-28 18:06:38 -0300

    Escondendo a senha

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { connect, Schema, model } = require('mongoose');
-let url = 'mongodb+srv://millieAdmin:Tornado135@milliebot-v0jnh.gcp.mongodb.net/test?retryWrites=true&w=majority'
+let url = 'mongodb+srv://NUMPODI:NUMPODI@NUMPODI-v0jnh.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 connect(url, { useNewUrlParser: true }, (err) => {
     if (err) return console.log('[DataBase/Error] > Erro ao tentar conectar à DB.')
